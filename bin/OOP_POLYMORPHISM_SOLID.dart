8bda6cd9e955eacf49a063e0a65baf0d3215c76b
Adly <85500151+AdlyRadwika@users.noreply.github.com>
2022-09-27 10:09:19 +0700

    Create OOP_POLYMORPHISM_SOLID.dart

---
+++ bin/OOP_POLYMORPHISM_SOLID.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+//RUNTIME POLYMORPHISM
+void main() {
+  var cat = Cat();
+  cat.walk();
+}
+ 
+class Animal {
+  void walk() {
+    print('Animal walk!');
+  }
+}
+ 
+class Cat extends Animal {
+  @override
+  void walk() {
+    super.walk();
+    print('Yeay! Cat walked!');
+  }
+}
