8ff3bb813b377f285e1228bd4ed4409d68e986fc
Adly <85500151+AdlyRadwika@users.noreply.github.com>
2022-09-30 09:03:17 +0700

    Create SOLID_DIP.dart

---
+++ bin/SOLID_DIP.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+//Dependency Inversion Principle
+
+//BEFORE
+/*
+class Car {
+  final Engine _engine;
+  Car(this._engine);
+ 
+  void start() {
+    _engine.start();
+  }
+}
+ 
+class Engine {
+  void start() {
+ 
+  }
+}
+*/
+
+//AFTER
+class DieselEngine {
+  void start() {
+ 
+  }
+}
+
+abstract class EngineInterface {
+  void start();
+}
+
+class Car {
+  final EngineInterface _engine;
+ 
+  Car(this._engine);
+ 
+  void start() {
+    _engine.start();
+  }
+}
+
+class PetrolEngine implements EngineInterface {
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
+ 
+class HybridEngine implements EngineInterface {
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
+ 
+class DieselEngine implements EngineInterface {
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
+
+void main() {
+  final petrolEngine = PetrolEngine();
+  final petrolCar = Car(petrolEngine);
+ 
+  final dieselEngine = DieselEngine();
+  final dieselCar = Car(dieselEngine);
+ 
+  final hybridEngine = HybridEngine();
+  final hybridCar = Car(hybridEngine);
+ 
+  petrolCar.start();
+  dieselCar.start();
+  hybridCar.start();
+}
